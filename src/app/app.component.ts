9484895c33b7dacede0d72f5d2b932738b0699ec
GowthamanM <gowthamancode@gmail.com>
2021-04-03 12:58:00 +0530

    Search Funtion added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
-
+import {Model}from './model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ang-Project-XV';
+  arraySize:boolean = false;
+  searchElement:any;
+  users:Array<Model>;
+  database:Array<Model>=[
+    {
+      "firstName":"Gowthaman",
+      "lastName":"M",
+      "age":"21",
+      "country":"India"
+    },{
+      "firstName":"Gokul",
+      "lastName":"Shakthi",
+      "age":"21",
+      "country":"India"
+    },{
+      "firstName":"Elon",
+      "lastName":"Musk",
+      "age":"41",
+      "country":"USA"
+    }
+  ];
+  constructor(){
+    this.users=this.database;
+  }
+  ngOnit(){}
+
+  search(){  
+    if(this.searchElement === ''){
+      this.users = this.database;
+    }else{
+      this.users = [];
+      this.database.forEach((data)=>{
+        if(data.firstName === this.searchElement)
+          this.users.push(data);
+          this.arraySize = false;
+      });
+      if(this.users.length ===0)
+        this.arraySize = true;
+    }
+    console.log(this.users);
+    
+    
+  }
+
 }
---
+++ src/app/model.ts
@@ -0,0 +1,6 @@
+export class Model {
+        firstName:any;
+        lastName:any;
+        age:any;
+        country:any;
+}
